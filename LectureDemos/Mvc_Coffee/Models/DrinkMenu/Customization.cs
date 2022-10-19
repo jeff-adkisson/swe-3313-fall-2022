52d43b0bff90658bff760b9866f152d010bfcfaf
Jeff Adkisson <Jeff@YellowFog.com>
2022-10-18 21:33:13 -0400

    Added MVC demo

---
+++ LectureDemos/Mvc_Coffee/Models/DrinkMenu/Customization.cs
@@ -0,0 +1,13 @@
+using System.Text.RegularExpressions;
+
+namespace Mvc_Coffee.Models.DrinkMenu;
+
+public class Customization
+{
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+    
+    public string Url => Regex.Replace(Name.Trim(), "[^a-zA-Z0-9]", "-");
+
+}
