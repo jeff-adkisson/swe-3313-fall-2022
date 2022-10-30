ad69c527ac7d03b2f6cc26b39a8fd363ee1aefc4
Garrett <garrettheffner27@gmail.com>
2022-10-30 18:35:49 -0400

    management and payment screens base

---
+++ Source/CoffeePointOfSale/Management.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeePointOfSale
+{
+    public partial class Management : Form
+    {
+        public Management()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Management_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
