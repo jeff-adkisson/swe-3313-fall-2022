193ac92255faba4ec8241ab6e1e6d3cf22895aa6
Garrett <garrettheffner27@gmail.com>
2022-10-30 18:56:05 -0400

    management screen changed

--- Source/CoffeePointOfSale/Forms/FormManagement.Designer.cs
+++ Source/CoffeePointOfSale/Forms/FormManagement.Designer.cs
@@ -29,47 +29,54 @@
         private void InitializeComponent()
         {
             this.btnClose = new System.Windows.Forms.Button();
-            this.txtDeleteThis = new System.Windows.Forms.TextBox();
-            this.lblRemoveThis = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(188, 34);
+            this.btnClose.BackColor = System.Drawing.Color.Firebrick;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(12, 11);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(94, 29);
+            this.btnClose.Size = new System.Drawing.Size(184, 70);
             this.btnClose.TabIndex = 0;
-            this.btnClose.Text = "Close";
-            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Text = "Go Back";
+            this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.OnClickBtnClose);
             // 
-            // txtDeleteThis
+            // button2
             // 
-            this.txtDeleteThis.AcceptsReturn = true;
-            this.txtDeleteThis.Location = new System.Drawing.Point(188, 110);
-            this.txtDeleteThis.Multiline = true;
-            this.txtDeleteThis.Name = "txtDeleteThis";
-            this.txtDeleteThis.Size = new System.Drawing.Size(550, 100);
-            this.txtDeleteThis.TabIndex = 1;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(459, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(397, 158);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Generate Report";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // lblRemoveThis
+            // label1
             // 
-            this.lblRemoveThis.AutoSize = true;
-            this.lblRemoveThis.Location = new System.Drawing.Point(188, 87);
-            this.lblRemoveThis.Name = "lblRemoveThis";
-            this.lblRemoveThis.Size = new System.Drawing.Size(177, 20);
-            this.lblRemoveThis.TabIndex = 2;
-            this.lblRemoveThis.Text = "Demo - Remove This Box";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(437, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 89);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Management";
             // 
             // FormManagement
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.ClientSize = new System.Drawing.Size(1332, 682);
             this.ControlBox = false;
-            this.Controls.Add(this.lblRemoveThis);
-            this.Controls.Add(this.txtDeleteThis);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.btnClose);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormManagement";
@@ -84,7 +91,7 @@
         #endregion
 
         private Button btnClose;
-        private TextBox txtDeleteThis;
-        private Label lblRemoveThis;
+        private Button button2;
+        private Label label1;
     }
 }
